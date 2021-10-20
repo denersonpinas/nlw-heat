83dbec23836e575bd3c897eea332a431d1bfabf0
denersonpinas <denersonpinas19@gmail.com>
2021-10-20 12:38:22 -0300

    Integração do Backend com o Frontend Finalizada, iniciando a construção do Layout Funcional do Envio de Menssagem

--- web/src/components/LoginBox/index.tsx
+++ web/src/components/LoginBox/index.tsx
@@ -1,12 +1,16 @@
+import { useContext } from 'react';
 import { VscGithubInverted } from 'react-icons/vsc';
+import { AuthContext } from '../../contexts/auth';
 
 import styles from './styles.module.scss';
 
 export function LoginBox() {
+    const { singInUrl } = useContext(AuthContext);
+
     return (
         <div className={styles.LoginBoxWrapper}>
             <strong>Entre e compartilhe sua mensagem</strong>
-            <a href="#" className={styles.singInWithGithub}>
+            <a href={singInUrl} className={styles.singInWithGithub}>
                 <VscGithubInverted size="24"/>
                 Entrar com GitHub
             </a>
